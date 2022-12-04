38ccf75e60e35f4038d4b9a884e81b2997dc136b
Damian Montero <photoca@hotmail.com>
2022-12-03 20:43:43 -0500

    Getting the notification ready to go

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect,useState } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 import './App.css';
 
+/* 
+Example of how to call this by hitting and endpoint 
+  curl \
+  -H "Title: Title of Notification" \
+  -H "Priority: urgent" \
+  -H "Tags: warning,skull" \
+  -d "main message in notification" \
+  https://ntfy.sh/DamianMontero
+*/
+
+interface WebSocketMessage {
+  id: string
+  time: string
+  event: string
+  topic:string
+  title: string
+  message: string
+  tags: string
+}
+
 function App() {
+  const [notification,setNotification] = useState<string>("");
+
+  useEffect( () => {
+
+    const checkPermissiong = async () => {
+      const permission = await Notification.requestPermission();
+      if (Notification.permission !== 'granted') {
+        alert("You've denied notification so this demo will not work. ")
+      }
+    }
+
+    checkPermissiong();
+
+      const webSocket = new WebSocket('wss://ntfy.sh/DamianMontero/ws');
+      webSocket.onmessage = (e) => {
+        //  Eample of e.data = {"id":"9y6Kxu34FQHV","time":1669941518,"event":"message","topic":"DamianMontero","title":"title of dama","message":"message","tags":["warning"]}
+        const {id,time,event,topic,title,message,tags} = JSON.parse(e.data) as WebSocketMessage
+        if (title) {
+          const greeting = new Notification(title,{
+              body: message,
+              // you could read the picture from the message or have a hard coded one like this
+              icon: 'https://damianmontero.com/damian.jpg' 
+            });
+          greeting.addEventListener('click', function(){
+            // what to do when they click on the message. You could do things here, or simply open a new page.
+            setNotification("OMG! You totally clicked that notification. \nGood job!")
+          });
+          // For here 
+          setNotification(`We have received a new mssage with the title: ${title}`);
+        }
+      //});
+      }
+
+      return () => webSocket.close();
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>
+      Notification Example.
+    </h1>
+    {notification}
+
       </header>
     </div>
   );
